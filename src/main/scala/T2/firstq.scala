8dc3e71884877696803bb164d77d8ade9c2c6db4
damitha sahan <sahanchandrasena462@gmail.com>
2023-06-18 23:06:59 +0530

    second commit

--- src/main/scala/T2/firstq.scala
+++ src/main/scala/T2/firstq.scala
@@ -2,4 +2,20 @@ package T2
 
 object firstq {
 
+  def main(args: Array[String]): Unit = {
+
+    var k , i , j = 2;
+    var m , n = 5;
+    var f = 12.0f;
+    var g = 4.0f;
+    var c = 'X';
+
+    println()
+
+  }
+
+
+
+
+
 }
