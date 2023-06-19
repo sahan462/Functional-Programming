ef6750740d3a62da28836c578a6ebfcdfdb0f3be
damitha sahan <sahanchandrasena462@gmail.com>
2023-06-19 22:19:41 +0530

    sixth commit

--- src/main/scala/T2/fourthq.scala
+++ src/main/scala/T2/fourthq.scala
@@ -1,5 +1,54 @@
 package T2
-
+import scala.math.abs
 object fourthq {
 
-}
+  def main(args: Array[String]): Unit = {
+    print("Enter Ticket Price: ")
+    val price: Int = scala.io.StdIn.readInt()
+    print(price)
+    print(getProfit(price))
+  }
+
+  def getProfit(price : Double): List[Double] = {
+
+    val basePrice: Double = 15
+    val baseAttendance : Int = 120
+    var attendance : Int = 0;
+    var additional_amount : Int = 0
+    val rate : Int = 4
+    var normal_total_revenue : Double = 0
+    var abnormal_total_revenue : Double = 0
+    var profit_1 : Double = 0;
+
+    additional_amount = abs(basePrice - price).toInt
+    attendance = additional_amount * rate
+
+    if(price < basePrice){
+
+      normal_total_revenue = price * (baseAttendance + attendance)
+      profit_1 = normal_total_revenue - (500 + (baseAttendance + attendance) * 3)
+
+      val profits : List[Double] = List(profit_1)
+      return profits
+    }else{
+      var profit_2 : Double = 0
+
+      normal_total_revenue = price * (baseAttendance - attendance)
+      abnormal_total_revenue = price * (baseAttendance + attendance)
+
+      profit_1 = normal_total_revenue - (500 + (baseAttendance - attendance) * 3)
+      profit_2 = abnormal_total_revenue - (500 + (baseAttendance + attendance) * 3)
+
+
+      val profits: List[Double] = List(profit_1,profit_2)
+      return profits
+    }
+
+
+
+
+
+
+  }
+
+  }
