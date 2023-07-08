3e3eadcb6c40de0245087f2f8dc087d72de346a0
damitha sahan <sahanchandrasena462@gmail.com>
2023-07-08 23:56:05 +0530

    T5q2

--- src/main/scala/T5/secondq.scala
+++ src/main/scala/T5/secondq.scala
@@ -1,5 +1,37 @@
 package T5
+import scala.io.StdIn
 
 object secondq {
 
+  def primeSeq(n: Int): Unit = {
+    if(n==1){
+      return
+    }else{
+      def Prime(x: Int, y: Int): String = {
+        if (y == 1) {
+          return "true";
+        } else {
+          if (x % y == 0) {
+            return "false"
+          } else {
+            Prime(x, y - 1);
+          }
+        }
+      }
+      if (Prime(n,n-1)== "true"){
+        println(n)
+        primeSeq(n-1);
+      }else{
+        primeSeq(n-1)
+      }
+    }
+  }
+
+
+
+  def main(args: Array[String]): Unit = {
+    print("Enter Value: ");
+    val x = StdIn.readLine().toInt;
+    primeSeq(x);
+  }
 }
