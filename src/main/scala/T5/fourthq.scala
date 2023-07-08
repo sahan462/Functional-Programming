6806f25f76a5c5556fa855fd4657c7b2382a0ab1
damitha sahan <sahanchandrasena462@gmail.com>
2023-07-09 04:18:44 +0530

    T5q4

--- src/main/scala/T5/fourthq.scala
+++ src/main/scala/T5/fourthq.scala
@@ -1,5 +1,22 @@
 package T5
+import scala.io.StdIn
 
 object fourthq {
+
+  def check_even_odd(n: Int): String = {
+    if(n == 0){
+      return "Even"
+    }else if(n < 0){
+      return "ODD"
+    }else{
+      check_even_odd(n-2)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    print("Enter Value: ");
+    val x = StdIn.readLine().toInt;
+    println(check_even_odd(x));
+  }
 
 }
