bfea9ff78e5a2e85b7b753d28684e4f864f2a107
damitha sahan <sahanchandrasena462@gmail.com>
2023-07-09 02:10:27 +0530

    T5q3

--- src/main/scala/T5/thirdq.scala
+++ src/main/scala/T5/thirdq.scala
@@ -1,5 +1,20 @@
 package T5
+import scala.io.StdIn
 
 object thirdq {
+
+  def sum(n: Int): Int = {
+    if(n<=0){
+      return n
+    }else{
+      return n + sum(n-1)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    print("Enter Value: ");
+    val x = StdIn.readLine().toInt;
+    println(sum(x));
+  }
 
 }
