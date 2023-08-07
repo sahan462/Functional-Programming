046a188eb1d67f09ff594df52c9342e4aec4d4fd
damitha sahan <sahanchandrasena462@gmail.com>
2023-08-08 03:37:52 +0530

    t7q2

---
+++ src/main/scala/T7/secondq.scala
@@ -0,0 +1,17 @@
+package T7
+
+object secondq {
+
+  val squareNumbers = (list: List[Int]) => list.map(num => math.pow(num,2).toInt);
+
+  def calculateSquare(function: (List[Int]) => List[Int], l: List[Int]): Unit = {
+    println("List with square values: " + function(l));
+  }
+
+  def main(args: Array[String]): Unit = {
+    val list = List(1, 2, 3, 4, 5);
+    calculateSquare(squareNumbers, list);
+  }
+
+
+}
