e3750dd440cd77b0f9c54c651a2cdf67a86d076d
damitha sahan <sahanchandrasena462@gmail.com>
2023-08-25 13:07:13 +0530

    t9done

---
+++ src/main/scala/T9/fourthq.scala
@@ -0,0 +1,82 @@
+package T9
+
+package T9
+
+class Bank(accounts: List[Account]) {
+  def accountsWithNegativeBalances: List[Account] = {
+    accounts.filter(account => account.balance < 0)
+  }
+
+  def totalBalance: Double = {
+    accounts.map(_.balance).sum
+  }
+
+  def applyInterestToAccounts: List[Account] = {
+    for (account <- accounts) {
+      if (account.balance > 0) {
+        account.deposit(account.balance * 0.05) // Deposit interest of 5% for positive balances
+      } else {
+        account.deposit(account.balance * 0.01) // Overdraft interest of 1% for negative balances
+      }
+    }
+    accounts
+  }
+}
+
+class Account(nic: String, val accId: Int, var balance: Double = 0.0) {
+
+  def deposit(amount: Double): Unit = {
+    this.balance = this.balance + amount
+  }
+
+  def withdraw(amount: Double): Unit = {
+    this.balance = this.balance - amount
+  }
+
+  def transfer(account: Account, amount: Double): Unit = {
+    if (this.balance < amount) {
+      println("\n" + "Insufficient Balance!!")
+    } else {
+      println("This account current balance: " + this.balance)
+      println("Given account current balance: " + account.balance)
+
+      this.withdraw(amount)
+      account.deposit(amount)
+
+      println("This account updated balance: " + this.balance)
+      println("Given account updated balance: " + account.balance)
+    }
+  }
+
+  def getNIC(): String = {
+    return this.nic
+  }
+
+  def getBalance(): Double = {
+    return this.balance
+  }
+}
+
+object fourthq {
+  def main(args: Array[String]): Unit = {
+
+    var account1 = new Account("200130003276", 1, 1000.0)
+    var account2 = new Account("200130003277", 2, -500.0)
+    var account3 = new Account("200130003278", 3, 200.0)
+    var account4 = new Account("200130003279", 4, -100.0)
+
+    val accounts = List(account1, account2, account3, account4);
+    val bank = new Bank(accounts)
+
+    val negativeBalanceAccounts = bank.accountsWithNegativeBalances
+    println("Accounts with negative balances:")
+    negativeBalanceAccounts.foreach(account => println(s"Account ${account.getNIC()}: ${account.getBalance()}"))
+
+    val totalBalances = bank.totalBalance
+    println(s"Total balance of all accounts: $totalBalances")
+
+    val updatedAccounts = bank.applyInterestToAccounts
+    println("Updated account balances after applying interest:")
+    updatedAccounts.foreach(account => println(s"Account ${account.getNIC()}: ${account.getBalance()}"))
+  }
+}
--- src/main/scala/T9/secondq.scala
+++ src/main/scala/T9/secondq.scala
@@ -1,5 +1,50 @@
 package T9
 
-object secondq {
+class Rational_Q2 {
+ private var numerator: Int = 0
+ private var denominator: Int = 1
 
+ def this(numerator: Int, denominator: Int) = {
+  this()
+  try {
+   require(denominator != 0, "Denominator cannot be zero")
+   this.numerator = numerator
+   this.denominator = denominator
+  } catch {
+   case e: IllegalArgumentException =>
+    println(e.getMessage)
+  }
+ }
+
+ def neg: Rational_Q2 = {
+  val neg_ob = new Rational_Q2(-this.numerator, this.denominator)
+  println(neg_ob.numerator + "/" + neg_ob.denominator)
+  neg_ob
+ }
+
+ def -(that: Rational_Q2): Rational_Q2 = {
+  val commonDenominator = this.denominator * that.denominator
+  val newNumerator = (this.numerator * that.denominator) - (that.numerator * this.denominator)
+  new Rational_Q2(newNumerator, commonDenominator)
+ }
+
+ def getNumerator(): String = {
+  return this.numerator.toString
+ }
+
+ def getDenominator(): String = {
+  return this.denominator.toString
+ }
+
+}
+
+object secondq {
+ def main(args: Array[String]): Unit = {
+  val x = new Rational_Q2(3, 4)
+  val y = new Rational_Q2(5, 8)
+  val z = new Rational_Q2(2, 7)
+
+  val result = x - y - z
+  println(result.getNumerator() + "/" + result.getDenominator())
+ }
 }
