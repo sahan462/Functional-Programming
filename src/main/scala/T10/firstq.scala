b6b0b993c3e00b6ab2fc8b923ff917baf78b25e6
damitha sahan <sahanchandrasena462@gmail.com>
2023-08-29 07:14:42 +0530

    t10q1

--- src/main/scala/T10/firstq.scala
+++ src/main/scala/T10/firstq.scala
@@ -2,4 +2,18 @@ package T10
 
 object firstq {
 
+  def calculateAverage(temperatureList: List[Int]): Unit = {
+
+    val fahrenheitList = temperatureList.map(element => element * (9 / 5.0) + 32);
+    val average = fahrenheitList.reduce((x, y) => x + y) / fahrenheitList.length;
+    print(average);
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val celciusList = List(0, 10, 20, 30);
+    calculateAverage(celciusList);
+
+  }
+
 }
