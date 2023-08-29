aa5ade99224062dad289e23a2f40333bb4ae08d0
damitha sahan <sahanchandrasena462@gmail.com>
2023-08-29 07:24:34 +0530

    t10q2

--- src/main/scala/T10/secondq.scala
+++ src/main/scala/T10/secondq.scala
@@ -2,4 +2,19 @@ package T10
 
 object secondq {
 
+  def countLetterOccurrences(nameList: List[String]): Unit = {
+
+    val nameLengthList = nameList.map(element => element.length);
+    val letterCount = nameLengthList.reduce((x, y) => x + y) ;
+    print("Total count of letter occurrences: " + letterCount);
+
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val nameList = List("apple", "banana", "cherry", "date");
+    countLetterOccurrences(nameList);
+
+  }
+
 }
